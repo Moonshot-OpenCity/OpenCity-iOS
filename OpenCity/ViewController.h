5d20250cd21e746f0ccba4e863988236c38b730d
antonhansel <antoninribeaud@gmail.com>
2014-09-06 14:48:10 +0200

    First commit with base app for OpenCity

---
+++ OpenCity/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OpenCity
+//
+//  Created by Apollo on 06/09/14.
+//  Copyright (c) 2014 com.iosHello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OpenCityTests/OpenCityTests.m
@@ -0,0 +1,34 @@
+//
+//  OpenCityTests.m
+//  OpenCityTests
+//
+//  Created by Apollo on 06/09/14.
+//  Copyright (c) 2014 com.iosHello. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OpenCityTests : XCTestCase
+
+@end
+
+@implementation OpenCityTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
